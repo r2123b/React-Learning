43814604b36ea3fd1d99ded837fbf2c810ef520d
KYLU <r2123b@gmail.com>
2021-03-09 16:30:20 +0800

    add fetch() comments

--- src/main/main.js
+++ src/main/main.js
@@ -28,8 +28,19 @@ products.className = "products";
 main.appendChild(products);
 
 let src = "https://cwpeng.github.io/live-records-samples/data/products.json"
-// ajax
-fetch(src).then((response) => {
+// The fetch() method is used to send the requests to the server without refreshing the page
+/* basic syntax:
+fetch(url, {options}).then((respons) => {
+  return respons.json(); //取的資料後將資料傳給下一個 then
+}).then((data) => {
+  ...
+}).catch((error) => { // 當初出現錯誤時跑 catch
+  console.log(error);
+})
+ */
+
+// why not use jOuery ajax or Promises??
+fetch(src, ).then((response) => {
     return response.json();
 }).then((data) => {
     for (let i = 0; i < data.length; i++) {
