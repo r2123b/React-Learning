17a0f777d8a1a4181f0547189254b57ba7168b13
KYLU <r2123b@gmail.com>
2021-03-10 21:21:12 +0800

    modify welcome objecy by React

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
-import nav from "./nav/nav.js"
+import React from "react"
+import ReactDOM from "react-dom"
+
+//import nav from "./nav/nav.js"
 import welcome from "./welcome/welcome.js"
-import main from "./main/main.js"
+//import main from "./main/main.js"
 
 // 也把 .css 交給 webpack 管理 (即把 .css 放在 src folder 裡面)
 // 但需要在 "webpack.config.js" 裡面加入 module
 import "./index.css"
 
-document.body.appendChild(nav);
-document.body.appendChild(welcome);
-document.body.appendChild(main);
+ReactDOM.render(
+    welcome,
+    document.querySelector("#root")    // 不直接畫在 root 底下
+);
--- src/welcome/welcome.js
+++ src/welcome/welcome.js
@@ -1,6 +1,7 @@
 import "./welcome.css"
+import React from "react"
 
-/* 舊版的：
+/* version 1：
 
 in "index.html":
 <div class="welcome">
@@ -19,6 +20,7 @@ welcome.addEventListener("click", function () {
 })
 */
 
+/* version 2
 let welcome = document.createElement("div");
 welcome.className = "welcome";
 welcome.textContent = "歡迎光臨";
@@ -30,8 +32,29 @@ welcome.addEventListener("click", function () {
         welcome.textContent = "再次見到你了";
     flip = !flip;
 });
+*/
+
+// React.createElement("標籤名稱", 屬性, sub-components);
+// this is to create "React element" not "HTML element"
+// React can lead to an error, "Node.appendChild must be an instance of Node"
+let flip = true;
+let welcome = React.createElement(
+    "div",
+    {
+        className: "welcome",
+        onClick: function () {
+            if (flip)
+                document.querySelector(".welcome").textContent = "很高興見到你";
+            else
+                document.querySelector(".welcome").textContent = "再次見到你了";
+            flip = !flip;
+        }
+    },
+    "歡迎光臨"
+);
+
+
 
 // document.body.appendChild(welcome); -> 這會影響到 index.html <body> 的排版
 // 所以用底下取代
-
 export default welcome;
